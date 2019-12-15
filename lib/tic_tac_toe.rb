8777bdbf2f85540d5f26b6fb357f4f95a6a4c275
Tom Banish <tombanish@gmail.com>
2019-12-15 19:29:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -114,9 +114,9 @@ end
 def winner
   WIN_COMBINATIONS.each do |win_combo|
     if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X" 
-		  "X"
+		  return "X"
 		elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O" 
-	    "O"
+	    return "O"
 		end
 	end
 	nil
