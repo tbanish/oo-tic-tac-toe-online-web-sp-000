81ae0975c21d40b96dd495b81345625ad1f68d19
Tom Banish <tombanish@gmail.com>
2019-12-15 18:53:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,6 +23,8 @@ WIN_COMBINATIONS = [
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
+user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
