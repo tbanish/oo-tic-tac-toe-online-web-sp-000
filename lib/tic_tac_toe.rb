0d1de85c737bcad838edbfd03be2262e53cdb40f
Tom Banish <tombanish@gmail.com>
2019-12-15 18:55:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,7 +10,9 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [6, 4, 2]
 ]
-  
+
+@user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
@@ -23,12 +25,12 @@ WIN_COMBINATIONS = [
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
-@user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def input_to_index(user_input)
   @user_input = user_input
   user_input.to_i - 1
 end
+
 
 def move(board, user_input, char)
   board[user_input] = char
