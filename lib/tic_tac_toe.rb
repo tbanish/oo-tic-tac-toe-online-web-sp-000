c2c141064821a13c715d552a3dd181bf79fe6dff
Tom Banish <tombanish@gmail.com>
2019-12-15 19:04:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,7 +46,111 @@ def position_taken?(position)
   end
 end
 
+def valid_move?(position)
+  if position_taken?(@board, position) == false && position.between?(0,8) == true
+    true
+  else position_taken?(@board, position) == true
+    false
+  end
+end
 
+
+# def turn
+#   puts "Please enter 1-9:"
+#   user_input = gets.strip
+#   user_input = input_to_index(user_input)
+#   char = current_player(@board)
+#   until valid_move?(board, user_input) == true
+#       turn(board)
+#       break
+#     end
+#   if valid_move?(board, user_input)
+#     move(board, user_input, char)
+#     display_board(board)
+#   end
+# end
+
+
+# #turn_count
+# def turn_count(board)
+#   number_of_turns = 0
+#   board.each do |token|
+#     if token == "X" || token == "O"
+#     number_of_turns += 1
+#     end
+#   end
+#   return number_of_turns
+# end
+
+
+# #current_player
+# def current_player(board)
+#   turn_count(board).even? ? "X" : "O"
+# end
+
+
+# #won?
+# def won?(board) 
+# 	WIN_COMBINATIONS.each do |win_combo| 
+# 		if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X" 
+# 			return win_combo 
+# 		elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O" 
+# 			return win_combo
+# 		end
+# 	end
+# 	false
+# end	
+
+# #full?
+# def full?(board)
+#   if board.include?(" ") || board.include?("")
+#     return false
+#   else
+#     return true
+#   end
+# end
+
+# #draw?
+# def draw?(board)
+#   if !won?(board) && full?(board)
+#     return true
+#   end
+# end
+
+# #game over?
+# def over?(board)
+#   if won?(board) || draw?(board) || full?(board)
+#     return true
+#   else
+#     return false
+#   end
+# end
+
+# #who won?
+# def winner(board)
+#   WIN_COMBINATIONS.each do |win_combo|
+#     if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X" 
+# 			return "X"
+# 		elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O" 
+# 			return "O"
+# 		end
+# 	end
+# 	nil
+# end
+
+
+# def play(board)
+#   until over?(board) == true
+#     turn(board)
+#   end
+#   if winner(board) == "X"
+#     puts "Congratulations X!"
+#   elsif winner(board) == "O"
+#     puts "Congratulations O!"
+#   elsif draw?(board)
+#     puts "Cat's Game!"
+#   end
+# end
 
 
 
